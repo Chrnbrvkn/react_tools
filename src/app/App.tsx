7e3f44bb5c5d48ace34bf3a199c92b360e953bcc
yan <yan428@mail.ru>
2024-05-13 00:25:41 +0300

    Routes Config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { MainPage } from "pages/MainPage";
 import { AboutPage } from "pages/AboutPage";
+import {AppRouter} from "app/providers/router";
 
 export default function App() {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +17,7 @@ export default function App() {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Home page</Link>
       <Link to={"/about"}>About page</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+        <AppRouter />
     </div>
   );
 }
